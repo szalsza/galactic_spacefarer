688d1fd2f224f0bd8e4167a0f5d0fe56c6b43a36
szalsza <szmenesi@gmail.com>
2025-10-20 18:36:52 +0200

    first commit

---
+++ srv/src/main/java/customer/galactic_spacefarer/Application.java
@@ -0,0 +1,13 @@
+package customer.galactic_spacefarer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
